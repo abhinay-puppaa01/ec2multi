d6d5d3daa89aa70b6f02f0688b1e0f44fd12f695
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-23 18:43:19 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -82,7 +82,6 @@ output "machine_values" {
   description = "Alternatively way  of resource values."
   value = { for k, v in aws_instance.ec2_test : k => {
     name = v.private_ip
-    coun = "${k}"
     machine_id   = v.id
     machine_state = v.instance_state
     resource_name = v.tags.Name
@@ -92,20 +91,15 @@ output "machine_values" {
     vpc_id = v.vpc_security_group_ids
     subnet_id = v.subnet_id
     network_interface_id = v.primary_network_interface_id
-    root_block_device_volume_id = v.root_block_device[*]
-    ebs_block_device_volume_id = v.ebs_block_device[*]
+    root_block_device_device_name = v.root_block_device[*].device_name
+    root_block_device_volume_id = v.root_block_device[*].volume_id
+    root_block_device_volume_size = v.root_block_device[*].volume_size
+    root_block_device_volume_type = v.root_block_device[*].volume_type
+    ebs_block_device_device_name = v.ebs_block_device[*].device_name
+    ebs_block_device_volume_id = v.ebs_block_device[*].volume_id
+    ebs_block_device_volume_size = v.ebs_block_device[*].volume_size
+    ebs_block_device_volume_type = v.ebs_block_device[*].volume_type
     ec2_test_tags_all = v.tags
     }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
